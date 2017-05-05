5e2e57bdb910d09fbc217686be05ef430edf6181
xxxgitone <xxxgit@sina.com>
2017-05-05 22:39:55 +0800

    模拟数据

---
+++ server/data/comment.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    by: '590c62b86f8bd825cc366b80',
+    text: '1分27秒，不谢',
+    supporter: 24,
+    created_at: 1493990546466,
+    childrens: ['590c7ab01fa1482d7473ee65', '590c7cfc80cfd1120c961858']
+  },
+  {
+    by: '590c62b86f8bd825cc366b6d',
+    text: '老司机',
+    supporter: 5,
+    created_at: 1493990587649,
+    childrens: [],
+    parent: '590c79d37b92381b887e928e'
+  },
+  {
+    by: '590c62b86f8bd825cc366b7e',
+    text: '好人一生平安',
+    supporter: 7,
+    childrens: [],
+    parent: '590c79d37b92381b887e928e'
+  },
+  {
+    by: '590c5c8a2c353125ac662414',
+    text: '四月份航母下水，五月份飞机上天，接下来该轰20登场了吧！',
+    supporter: 241,
+    childrens: [],
+    parent: ''
+  }
+]
---
+++ server/data/user.js
@@ -0,0 +1,184 @@
+module.exports = [
+  {
+    name: '醉美祁连',
+    avatar_url: 'http://p3.pstatp.com/large/e590016d9c1389e2290',
+    created_at: 1493981236548,
+    followers: 1500,
+    following: 3
+  },
+  {
+    name: '三农故事汇',
+    avatar_url: 'http://p6.pstatp.com/large/123300125e8740977802',
+    created_at: 1493983654875,
+    followers: 15000,
+    following: 0
+  },
+  {
+    name: 'xhauf',
+    avatar_url: 'http://p3.pstatp.com/large/1782000bf2c6e84d5858',
+    created_at: 1493983654875,
+    followers: 508,
+    following: 57
+  },
+  {
+    name: '搞笑了',
+    avatar_url: 'http://p1.pstatp.com/large/12340014b2156801410d',
+    created_at: 1493983654875,
+    followers: 562,
+    following: 4
+  },
+  {
+    name: '失望',
+    avatar_url: 'http://p3.pstatp.com/large/16aa0006ff5e5812f636',
+    created_at: 1493983654875,
+    followers: 4857,
+    following: 422
+  },
+  {
+    name: '付款好的',
+    avatar_url: 'http://p3.pstatp.com/large/16aa000096b9e609b8af',
+    created_at: 1493983654875,
+    followers: 8677,
+    following: 2
+  },
+  {
+    name: '范德萨',
+    avatar_url: 'http://p3.pstatp.com/large/97e0014f626b5b99a5e',
+    created_at: 1493983654875,
+    followers: 45666,
+    following: 23
+  },
+  {
+    name: '福瑞迪',
+    avatar_url: 'http://p3.pstatp.com/large/18a3001274935c076013',
+    created_at: 1493983654875,
+    followers: 12,
+    following: 2
+  },
+  {
+    name: '地方',
+    avatar_url: 'http://p7.pstatp.com/large/1bf5001a81019f1d2cab',
+    created_at: 1493983654875,
+    followers: 123,
+    following: 2
+  },
+  {
+    name: '水电费',
+    avatar_url: 'http://p3.pstatp.com/large/97e00198226b86d03fe',
+    created_at: 1493983654875,
+    followers: 78,
+    following: 56
+  },
+  {
+    name: '温柔',
+    avatar_url: 'http://p3.pstatp.com/large/150d00150755fc024eb9',
+    created_at: 1493983654875,
+    followers: 7856,
+    following: 74
+  },
+  {
+    name: '温柔i',
+    avatar_url: 'http://p9.pstatp.com/large/150d00114d0c1fddfdfc',
+    created_at: 1493983654875,
+    followers: 776,
+    following: 6
+  },
+  {
+    name: '部分',
+    avatar_url: 'http://p1.pstatp.com/large/bc20000b91968707dab',
+    created_at: 1493983654875,
+    followers: 7896,
+    following: 54
+  },
+  {
+    name: '而他',
+    avatar_url: 'http://p3.pstatp.com/large/135300116e064d46c010',
+    created_at: 1493983654875,
+    followers: 785,
+    following: 24
+  },
+  {
+    name: '从vb',
+    avatar_url: 'http://p3.pstatp.com/large/1354000d4791d4ce2f56',
+    created_at: 1493983654875,
+    followers: 4,
+    following: 1
+  },
+  {
+    name: '门口集合',
+    avatar_url: 'http://p1.pstatp.com/large/ef400039e0ee42afa42',
+    created_at: 1493983654875,
+    followers: 7455,
+    following: 7
+  },
+  {
+    name: '敢不敢',
+    avatar_url: 'http://p1.pstatp.com/large/e5800131f4a71a58e71',
+    created_at: 1493983654875,
+    followers: 75,
+    following: 5
+  },
+  {
+    name: 'I机价格',
+    avatar_url: 'http://p3.pstatp.com/large/150e0001f89b83be9529',
+    created_at: 1493983654875,
+    followers: 7895,
+    following: 4
+  },
+  {
+    name: '梵蒂冈',
+    avatar_url: 'http://p1.pstatp.com/large/18a1000b1ce6f9a96048',
+    created_at: 1493983654875,
+    followers: 78,
+    following: 58
+  },
+  {
+    name: '改变',
+    avatar_url: 'http://p1.pstatp.com/large/97e000762fb0c68705e',
+    created_at: 1493983654875,
+    followers: 3,
+    following: 3
+  },
+  {
+    name: '的方式',
+    avatar_url: 'http://p3.pstatp.com/large/78f0000e14dd1e0a32a',
+    created_at: 1493983654875,
+    followers: 35,
+    following: 5
+  },
+  {
+    name: '山地车',
+    avatar_url: 'http://p3.pstatp.com/large/11804/901482813',
+    created_at: 1493983654875,
+    followers: 456,
+    following: 85
+  },
+  {
+    name: '下次V',
+    avatar_url: 'http://p3.pstatp.com/large/150d001111c603be971b',
+    created_at: 1493983654875,
+    followers: 789,
+    following: 32
+  },
+  {
+    name: 'vb好',
+    avatar_url: 'http://p3.pstatp.com/large/ef60000249ab16d1bf8',
+    created_at: 1493983654875,
+    followers: 485,
+    following: 5
+  },
+  {
+    name: 'vue',
+    avatar_url: 'http://p3.pstatp.com/large/150e0003355f00d49546',
+    created_at: 1493983654875,
+    followers: 58,
+    following: 55
+  },
+  {
+    name: 'javascript',
+    avatar_url: 'http://p3.pstatp.com/large/16ab000673f13f9293e0',
+    created_at: 1493983654875,
+    followers: 45,
+    following: 5
+  }
+]
