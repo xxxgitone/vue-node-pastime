35e356126d695d7a2f91d08ef59b01c611cbebfc
xxxgitone <xxxgit@sina.com>
2017-05-07 20:52:46 +0800

    测试api

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -25,8 +25,14 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      users: []
     }
+  },
+  created () {
+    this.$http.get('/api/users').then(data => {
+      this.users = data
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
