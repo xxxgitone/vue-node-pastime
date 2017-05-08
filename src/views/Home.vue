1767029ef0f2d707d5910a50b3d9f810c0f54e96
xxxgitone <xxxgit@sina.com>
2017-05-08 23:02:50 +0800

    home页

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header class="land">
+      
+    </header>
+    <section>
+      fdg
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.land {
+  height: 100vh;
+  width: 100%;
+  background-image: url('https://drscdn.500px.org/photo/210954383/q%3D80_m%3D1500/a94e5951e20c2871e0ee36b4f3453324');
+  background-size: cover;
+}
+</style>
