251e6d3e7e5b0e6a658e3c6a9674f827ba52e4e5
xxxgitone <xxxgit@sina.com>
2017-05-16 21:24:09 +0800

    视频播放

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import VideoList from '@/views/VideoList'
+import VideoPlayer from '@/views/VideoPlayer'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/videolist',
       name: 'videolist',
       component: VideoList
+    },
+    {
+      path: '/video/:id',
+      name: 'videoPlayer',
+      component: VideoPlayer
     }
   ]
 })
