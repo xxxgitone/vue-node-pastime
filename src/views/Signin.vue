50fbcb92a60fbc2bb3f12763755d06cb7c79dc0a
xxxgitone <xxxgit@sina.com>
2017-06-04 22:32:15 +0800

    编写样式

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="signin">
-      form
+      <form class="signinForm">
+        form
+      </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'signin'
+  name: 'signin',
+  mounted () {
+    this.$store.state.isHome = false
+  }
 }
 </script>
 
 <style lang="scss">
+.signin {
+  min-height: calc(100vh - 7rem);
+  padding-top: 3.75rem;
+  width: 30rem;
+  // border: 1px solid red;
+  margin: 0 auto;
+  background: rgba(0,0,0,.1);
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+  // flex-direction: column;
+}
 
 </style>
 
