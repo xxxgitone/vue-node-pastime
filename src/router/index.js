668578cfd37ab901f3446f6d067ce99873fcb3d1
xxxgitone <xxxgit@sina.com>
2017-06-05 17:10:19 +0800

    删除登录路由，改用弹窗

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Home from '@/views/Home'
 import VideoList from '@/views/VideoList'
 import VideoPlayer from '@/views/VideoPlayer'
-import Signin from '@/views/Signin'
 
 Vue.use(Router)
 
@@ -24,11 +23,6 @@ export default new Router({
       path: '/video/:id',
       name: 'videoPlayer',
       component: VideoPlayer
-    },
-    {
-      path: '/signin',
-      name: 'signin',
-      component: Signin
     }
   ]
 })
