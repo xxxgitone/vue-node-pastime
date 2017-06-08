0d2b4398c9d2bf53fa71f30761e4e3082b0cfbcc
xxxgitone <xxxgit@sina.com>
2017-06-08 21:21:32 +0800

    新建视频发布页面

---
+++ src/views/VideoPost.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>视频发布</div>
+    <div>视频发布</div>
+    <div>视频发布</div>
+    <div>视频发布</div>
+    <div>视频发布</div>
+    <div>视频发布</div>
+    <div>视频发布</div>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'videopost'
+}
+</script>
+
+<style lang="scss">
+
+</style>
