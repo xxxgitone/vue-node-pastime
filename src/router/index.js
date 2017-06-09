c61b65ab14d6cf8e812f074382c3d3aac28d3dc1
xxxgitone <xxxgit@sina.com>
2017-06-09 21:23:12 +0800

    谈价拦截器

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,17 @@ export default new Router({
     {
       path: '/post/video',
       name: 'videopost',
-      component: VideoPost
+      component: VideoPost,
+      beforeEnter: (to, from, next) => {
+        const token = localStorage.getItem('vn-token')
+        if (to.path === '/post/video') {
+          if (token !== 'null' && token !== null) {
+            next() // 登录了的话直接跳转
+          } else {
+            next('/') // 否则回到主页面
+          }
+        }
+      }
     }
   ]
 })
