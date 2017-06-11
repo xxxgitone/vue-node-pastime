524ed82ab06e20cfd788107f277cfacfb2fe5e5c
xxxgitone <xxxgit@sina.com>
2017-06-11 22:57:33 +0800

    添加全局拦截器

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/store'
-import axios from 'axios'
+import axios from './utils/axiosService'
 import moment from 'moment'
 
 Vue.config.productionTip = false
@@ -21,6 +21,20 @@ Vue.filter('durationFormat', duration => {
   const s = duration % 60
   const time = s >= 10 ? `${min}:${s}` : `${min}:0${s}`
   return time
+})
+
+// vue全局拦截器
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) { // 如果需要权限
+    if (store.state.token) { // 存在的话，直接跳转过去
+      next()
+    } else {
+      next({ path: '/' })
+      store.commit('SHOW_SIGNIN_DIALOG')
+    }
+  } else {
+    next()
+  }
 })
 
 /* eslint-disable no-new */
