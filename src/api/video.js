f12b87a6edee55eefc1e782cd862e9ab941d4d26
xxxgitone <xxxgit@sina.com>
2017-06-13 20:59:37 +0800

    增加api

--- src/api/video.js
+++ src/api/video.js
@@ -1,5 +1,9 @@
 import axios from '../utils/axiosService'
 
+export const fetchVideos = () => {
+  return axios.get('/api/videos')
+}
+
 export const fetchVideoById = (id) => {
   return axios.get(`/api/videos/${id}`)
 }
