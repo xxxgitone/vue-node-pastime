85890c44e5a0197583c723c1c434579d0355f254
xxxgitone <xxxgit@sina.com>
2017-06-13 19:35:25 +0800

    将个人菜单分离成组件

--- src/App.vue
+++ src/App.vue
@@ -21,18 +21,7 @@
             @blur="showSearch" 
             type="text" placeholder="search">
         </div>
-        <div class="personalMenu" v-show="token">
-          <img class="personalAvatar" :src="user.avatar_url" @click.stop="showMenu" />
-          <nav class="menu" v-show="showMenued">
-            <ul>
-              <li>
-                <router-link :to="'/post/video'">发布视频</router-link>
-                <a href="#">个人主页</a>
-                <a href="#" @click.prevent="signout">退出登录</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <PersonalMenu v-show="token" :user="user" ></PersonalMenu>
       </div>
     </header>
     <router-view></router-view>
@@ -69,11 +58,13 @@
 
 <script>
 import Signin from './components/Signin'
+import PersonalMenu from './components/personalMenu'
 import { mapState } from 'vuex'
 export default {
   name: 'app',
   components: {
-    Signin
+    Signin,
+    PersonalMenu
   },
   data () {
     return {
@@ -112,25 +103,10 @@ export default {
     // 显示登录注册框
     signin () {
       this.$store.commit('SHOW_SIGNIN_DIALOG')
-    },
-    // 显示个人菜单
-    showMenu (e) {
-      console.log(this)
-      if (e.target.className !== 'personalAvatar') {
-        this.showMenued = false
-      } else if (e.target.className === 'personalAvatar') {
-        this.showMenued = !this.showMenued
-      }
-    },
-    signout () {
-      this.$store.commit('SET_USER_OUT')
-      localStorage.setItem('vn-token', '')
-      this.$router.push('/')
     }
   },
   mounted () {
     window.addEventListener('scroll', this.updateScrollTop)
-    document.addEventListener('click', this.showMenu)
   }
 }
 </script>
@@ -265,45 +241,6 @@ a {
     }
 
   }
-
-  .personalMenu {
-    width: 2rem;
-    height: 100%;
-    border-radius: 50%;
-    margin-top: 1.25rem;
-    position: relative;
-    cursor: pointer;
-
-    img {
-      max-width: 2.5rem;
-      height: 2.5rem;
-      border-radius: 50%;
-    }
-
-    .menu {
-      position: absolute;
-      width: 7rem;
-      text-align: center;
-      top: 85%;
-      right: -140%;
-      background: rgba(0, 0, 0, .5);
-      border-radius: 5px;
-
-      a {
-        color: white;
-        display: block;
-        padding: .5rem;
-        letter-spacing: 1px;
-        transition: all .3s;
-
-        &:hover {
-          background: red;
-        }
-      }
-
-    }
-  }
-
 }
 
 .footer {
---
+++ src/components/personalMenu.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="personalMenu">
+    <img class="personalAvatar" :src="user.avatar_url" @click.stop="showMenu" />
+      <nav class="menu" v-show="showMenued">
+        <ul>
+          <li>
+            <a href="#">我喜欢的</a>
+            <router-link :to="'/post/video'">发布视频</router-link>
+            <a href="#">个人主页</a>
+            <a href="#" @click.prevent="signout">退出登录</a>
+          </li>
+        </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menu',
+  props: ['user'],
+  data () {
+    return {
+      showMenued: false
+    }
+  },
+  methods: {
+    // 显示个人菜单
+    showMenu (e) {
+      if (e.target.className !== 'personalAvatar') {
+        this.showMenued = false
+      } else if (e.target.className === 'personalAvatar') {
+        this.showMenued = !this.showMenued
+      }
+    },
+    signout () {
+      this.$store.commit('SET_USER_OUT')
+      localStorage.setItem('vn-token', '')
+      // 如果该页面存在权限的话，在此页面登录将跳转到主页
+      if (this.$route.meta.requireAuth) {
+        this.$router.push('/')
+      }
+    }
+  },
+  mounted () {
+    document.addEventListener('click', this.showMenu)
+  }
+}
+</script>
+
+<style lang="scss">
+ .personalMenu {
+    width: 2rem;
+    height: 100%;
+    border-radius: 50%;
+    margin-top: 1.25rem;
+    position: relative;
+    cursor: pointer;
+
+    img {
+      max-width: 2.5rem;
+      height: 2.5rem;
+      border-radius: 50%;
+    }
+
+    .menu {
+      position: absolute;
+      width: 7rem;
+      text-align: center;
+      top: 85%;
+      right: -140%;
+      background: rgba(0, 0, 0, .5);
+      border-radius: 5px;
+
+      a {
+        color: white;
+        display: block;
+        padding: .5rem;
+        letter-spacing: 1px;
+        transition: all .3s;
+
+        &:hover {
+          background: red;
+        }
+      }
+
+    }
+  }
+</style>
