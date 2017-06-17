0975a997938559ed8b0d97c2ebf0bda3e7a1effb
xxxgitone <xxxgit@sina.com>
2017-06-17 20:42:17 +0000

    Ubuntu迁移至deepin，测试

--- src/api/user.js
+++ src/api/user.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: xuthus 
+ * @Date: 2017-06-17 20:35:04 
+ * @Last Modified by:   xuthus 
+ * @Last Modified time: 2017-06-17 20:35:04 
+ */
 import axios from '../utils/axiosService' // 引入加了拦截器的axios
 
 export const signinByUsername = (username, password) => {
