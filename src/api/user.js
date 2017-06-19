edcaa5c742314d1cb4201187535b107eb100a8a1
xxxgitone <xxxgit@sina.com>
2017-06-19 19:44:16 +0800

    添加注释

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,8 @@
 /*
- * @Author: xuthus 
- * @Date: 2017-06-17 20:35:04 
- * @Last Modified by:   xuthus 
- * @Last Modified time: 2017-06-17 20:35:04 
+ * @Author: xuthus
+ * @Date: 2017-06-17 20:35:04
+ * @Last Modified by: xuthus
+ * @Last Modified time: 2017-06-18 23:34:21
  */
 import axios from '../utils/axiosService' // 引入加了拦截器的axios
 
