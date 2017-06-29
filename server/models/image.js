4d1b1568750ad44baafe18204752e502676d2928
xxxgitone <xxxgit@sina.com>
2017-06-29 22:17:38 +0800

    image Shcema

---
+++ server/models/image.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ImgaeSchema = new Schema({
+  user: {
+    _id: String,
+    name: String,
+    avatar_url: String
+  },
+  image_url: String,
+  created_at: { type: Date, default: Date.now() },
+  collections_count: Number,
+  description: String
+})
+
+const Image = mongoose.model('Imgae', ImgaeSchema)
+export default Image
