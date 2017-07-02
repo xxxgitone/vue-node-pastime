06da980e40270af1cc6f4a7c0e88f7e1509f66e9
xxxgitone <xxxgit@sina.com>
2017-07-02 21:03:22 +0800

    图片展示页面

--- src/views/ImageList.vue
+++ src/views/ImageList.vue
@@ -1,25 +1,46 @@
 <template>
   <div class="imagelist">
-    <div class="imageWrapper">
-      <img v-for="image in images" :src="image.image_url" :key="image._id">
-    </div> 
+    <div class="imagesWrapper">
+      <ImageItem v-for="image in images" :key="image._id" :image="image"></ImageItem>
+    </div>
+    <a href="#" class="loadMore" @click.prevent="fetchMore">加载更多</a>
   </div>
 </template>
 
 <script>
+import ImageItem from '../components/ImageItem'
 import { mapState } from 'vuex'
 export default {
   name: 'imagelist',
+  components: {
+    ImageItem
+  },
   created () {
-    this.$store.commit('FETCH_IMAGES')
+    this.$store.commit('FETCH_IMAGES', this.fetchCount)
   },
   computed: {
     ...mapState({
-      images: 'images'
+      images: 'images',
+      fetchCount: 'fetchCount',
+      isFetch: 'isFetch'
     })
+  },
+  methods: {
+    fetchMore () {
+      this.$store.state.fetchCount += 1
+      this.$store.commit('FETCH_IMAGES', this.fetchCount)
+    }
   },
   mounted () {
     this.$store.state.isHome = false
+    window.addEventListener('scroll', () => {
+      const scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+      // 页面滚动的高度 + 浏览器窗口的视口（viewport）高度 >= 整个页面可视区域高度 - 页面底部高度
+      if (scrollTop + window.innerHeight >= document.body.clientHeight - 90 && this.isFetch === false && this.fetchCount >= 2) {
+        this.isFetch = true
+        this.fetchMore()
+      }
+    })
   }
 }
 </script>
@@ -29,16 +50,24 @@ export default {
 
 .imagelist {
   @include stickFooter;
-  background: #eee;
+  text-align: center;
+  // background: #eee;
 
-  .imageWrapper {
+  .imagesWrapper {
     width: 90%;
     margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
   }
-  img {
-    height: 16.5rem;
-    width: auto;
-    margin: 1rem 0 0 .6rem;
+
+  .loadMore {
+    display: inline-block;
+    padding: .5rem;
+    margin: 1rem;
+    color: white;
+    background: #999;
+    border-radius: 5px;
   }
 }
 </style>
