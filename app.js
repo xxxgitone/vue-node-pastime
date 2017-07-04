42e3654bfc23a938ff9ac7ecec067b673226a939
xxxgitone <xxxgit@sina.com>
2017-07-04 20:05:39 +0800

    创建socket.io服务

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRoutes = require('./server/api/user')
 const videoRoutes = require('./server/api/video')
 const commentRoutes = require('./server/api/comment')
 const imageRoutes = require('./server/api/image')
+const socket = require('socket.io')
 
 const app = express()
 
@@ -26,6 +27,13 @@ app.use((err, req, res, next) => {
   res.status(442).send({ error: err.message })
 })
 
-app.listen(4000, () => {
+const server = app.listen(4000, () => {
   console.log(`Express started in ${app.get('env')} mode on http://localhsot:4000`)
+})
+
+// socket setup
+const io = socket(server)
+
+io.on('connect', (socket) => {
+  console.log('made socket connection', socket.id)
 })
