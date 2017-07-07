e6d53770f523499423b6954ba1230db67d9ac54f
xxxgitone <xxxgit@sina.com>
2017-07-07 16:04:28 +0800

    添加注释

--- src/api/user.js
+++ src/api/user.js
@@ -2,11 +2,12 @@
  * @Author: xuthus
  * @Date: 2017-06-17 20:35:04
  * @Last Modified by: xuthus
- * @Last Modified time: 2017-07-07 14:49:06
+ * @Last Modified time: 2017-07-07 16:04:10
  */
 import axios from '../utils/axiosService' // 引入加了拦截器的axios
 
-export const signinByUsername = (username, password, type) => {
+// 注册和登录api， 通过type判断
+export const signByUsername = (username, password, type) => {
   const data = {
     username,
     password,
