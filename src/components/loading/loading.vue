282d13ac1417f4738b4c6918ea8c5444c815f9cc
xxxgitone <xxxgit@sina.com>
2017-07-21 20:19:25 +0800

    loading组件

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="loading">
+    <img width="32" height="32" src="./32X32.gif">
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '数据加载中...'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .loading {
+    width: 100%;
+    text-align: center;
+
+    .desc {
+      font-size: 1rem;
+    }
+  }
+</style>
